a768822263672645fbd72b23d0d9d90a966cac1a
git config --global user.name balacek <david.bali@seznam.cz>
2019-09-05 18:15:36 +0200

    Change titles and styling on homepage

--- DOM_pig_game/app.js
+++ DOM_pig_game/app.js
@@ -15,11 +15,15 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
 document.querySelector("#current-" + activePlayer).textContent = dice;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-0").textContent = 0;
 
-function buttton() {}
-
-document.querySelector(".btn-roll").addEventListener("click", btn);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  dice = Math.floor(Math.random() * 6) + 1;
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
