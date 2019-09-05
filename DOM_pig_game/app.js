2ddd97d89fa0fbda5155cad22ddfc206103ed04f
git config --global user.name balacek <david.bali@seznam.cz>
2019-09-05 20:17:05 +0200

    init function of game

--- DOM_pig_game/app.js
+++ DOM_pig_game/app.js
@@ -11,15 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-0").textContent = 0;
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
   dice = Math.floor(Math.random() * 6) + 1;
@@ -40,9 +32,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
   scores[activePlayer] += roundScore;
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-  console.log("hh")
 
-  if (scores[activePlayer] >= 10) {
+  if (scores[activePlayer] >= 100) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -65,4 +56,30 @@ function nextPlayer() {
 
 
   document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+
+  document.querySelector("#current-" + activePlayer).textContent = dice;
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+
+  document.getElementById('name-0').textContent = 'Player 1 ';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
 }
