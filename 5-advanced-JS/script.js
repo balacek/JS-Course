455e3697ba3454e48a75d239fc8c2a6d74783726
git config --global user.name balacek <david.bali@seznam.cz>
2019-09-07 10:17:01 +0200

    prototype using, class object

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -1,4 +1,29 @@
 var john = {
-    name: 'John';
-
+    name: 'John',
+    yearOfBirth: 1998,
+    job: 'Teacher'
 }
+
+class Person {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+    calculateAge() {
+        console.log(2019 - this.yearOfBirth);
+    }
+}
+/*
+var Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function () {
+    console.log(2019 - this.yearOfBirth);
+}
+*/
+
+var mark = new Person('mark', 1847, 'Popelar');
