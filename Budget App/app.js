49695c04b24e06cfea558219e1e8bbfc977a01e9
git config --global user.name balacek <david.bali@seznam.cz>
2019-09-11 19:27:37 +0200

    created object for selectors

--- Budget App/app.js
+++ Budget App/app.js
@@ -4,27 +4,39 @@ var budgetController = (function () {
 
 
 var UIController = (function () {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: ''
+    };
 
     return {
         getinput: function () {
             return {
                 //will be inc or exp
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     }
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings;
+
     var ctrlAddItem = function () {
-        var input = UIController.getinput();
+        var input = UICtrl.getinput();
         console.log(input);
     }
 
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (e) {
         //which same as keycode but for older browsers
