869eb2856991ac2f141edbb34dab67dfc207e2f5
git config --global user.name balacek <david.bali@seznam.cz>
2019-09-12 18:12:19 +0200

    Clearing the values inputed and setting focus to first field

--- Budget App/app.js
+++ Budget App/app.js
@@ -92,6 +92,20 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function () {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach((current, index, array) => {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -123,6 +137,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 3. Add item to UI
         UICtrl.addListItem(newItem, input.type);
+
+        // 4. Clear the fields
+        UICtrl.clearFields();
 
     }
 
